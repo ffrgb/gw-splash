d3b90cec52efb0ba14ea52fea685f421e651bce1
Ben Oswald <ben.oswald@root-space.de>
2014-04-24 23:45:04 +0200

    rename database from nodes to clients

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///nodes.db')
+engine = create_engine('sqlite:///clients.db')
 Base = declarative_base()
 
-class Nodes(Base):
-    __tablename__ = 'nodes'
+class Clients(Base):
+    __tablename__ = 'clients'
 
     clientID = Column(Integer, primary_key=True)
     mac = Column(String, unique=True)
