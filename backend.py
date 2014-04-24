a709562a308f40e7526cc765162e0cfe351b08a3
Ben Oswald <ben.oswald@root-space.de>
2014-04-24 22:38:20 +0200

    add getMAC()

--- backend.py
+++ backend.py
@@ -1,16 +1,24 @@
 from flask import Flask, request, jsonify, render_template
 import json
+import re
+
 app = Flask(__name__)
+
+def getMAC(ip):
+    with open('/proc/net/arp') as arp:
+        for line in arp:
+            if re.search(ip, line):
+                return line.split()[3]
 
 @app.route('/', methods=['GET', 'POST'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST'])
 def main_site(path):
-    print(path)
+    mac = getMAC(request.remote_addr)
     token = False;
     print(request.url)
     if request.method == 'POST':
         token = True;
-        print(request.remote_addr)
+        print(mac)
     return render_template('toc.html', token = token)
 
 if __name__ ==  "__main__":
