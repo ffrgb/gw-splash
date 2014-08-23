0d83687d1e0566dd9422abe5487313440dc1d145
Ben Oswald <ben.oswald@root-space.de>
2014-08-23 12:03:22 +0200

    add IPv6 support

--- iptables.py
+++ iptables.py
@@ -8,40 +8,55 @@ class IPTables(object):
         self.chain = 'portal'
         self.iface = 'bat0'
 
-    def start(self):
-        call(['iptables', '-N', self.chain, '-t', 'mangle'])
-        call(['iptables', '-t', 'mangle', '-A', 'PREROUTING', '-i', self.iface, '-j', self.chain])
-        call(['iptables', '-t', 'mangle', '-A', self.chain, '-j', 'MARK', '--set-mark', self.mark])
-        call(['iptables', '-t', 'nat', '-A', 'PREROUTING', '-i', self.iface , '-m', 'mark', '--mark', self.mark, '-p', 'tcp', '--dport', '80', '-j', 'DNAT', '--to-destination', self.destination+':'+self.port])
-        call(['iptables', '-t', 'filter', '-A', 'FORWARD', '-m', 'mark', '--mark', self.mark, '-j', 'DROP'])
+    def __start(self, iptables):
+        call([iptables, '-N', self.chain, '-t', 'mangle'])
+        call([iptables, '-t', 'mangle', '-A', 'PREROUTING', '-i', self.iface, '-j', self.chain])
+        call([iptables, '-t', 'mangle', '-A', self.chain, '-j', 'MARK', '--set-mark', self.mark])
+        call([iptables, '-t', 'nat', '-A', 'PREROUTING', '-i',self.iface , '-m', 'mark', '--mark', self.mark, '-p', 'tcp', '--dport', '80', '-j', 'DNAT', '--to-destination', self.destination+':'+self.port])
+        call([iptables, '-t', 'filter', '-A', 'FORWARD', '-m', 'mark', '--mark', self.mark, '-j', 'DROP'])
 
-    def shutdown(self):
-        call(['iptables', '-t', 'mangle', '-D', 'PREROUTING', '-i', self.iface, '-j', self.chain])
-        call(['iptables', '-t', 'mangle', '-F', self.chain])
-        call(['iptables', '-t', 'mangle', '-X', self.chain])
-        call(['iptables', '-t', 'filter', '-D', 'FORWARD', '-m', 'mark', '--mark', self.mark, '-j', 'DROP'])
-        call(['iptables', '-t', 'nat', '-D', 'PREROUTING', '-i', self.iface , '-m', 'mark', '--mark', self.mark, '-p', 'tcp', '--dport', '80', '-j', 'DNAT', '--to-destination', self.destination+':'+self.port])
+    def __shutdown(self, iptables):
+        call([iptables, '-t', 'mangle', '-D', 'PREROUTING', '-i', self.iface, '-j', self.chain])
+        call([iptables, '-t', 'mangle', '-F', self.chain])
+        call([iptables, '-t', 'mangle', '-X', self.chain])
+        call([iptables, '-t', 'filter', '-D', 'FORWARD', '-m', 'mark', '--mark', self.mark, '-j', 'DROP'])
+        call([iptables, '-t', 'nat', '-D', 'PREROUTING', '-i',self.iface , '-m', 'mark', '--mark', self.mark, '-p', 'tcp', '--dport', '80', '-j', 'DNAT', '--to-destination', self.destination+':'+self.port])
 
-    def unlockMAC(self, mac):
-        if not call(['iptables', '-t', 'mangle', '-I', self.chain, '-m', 'mac', '--mac-source', mac, '-j', 'RETURN']):
+    def __unlockMAC(self, iptables, mac):
+        if not call([iptables, '-t', 'mangle', '-I', self.chain, '-m', 'mac', '--mac-source', mac, '-j', 'RETURN']):
             return True
         else:
             return False
 
-    def lockMAC(self, mac):
-        res = call(['iptables', '-t', 'mangle', '-D', self.chain, '-m', 'mac', '--mac-source', mac, '-j', 'RETURN'])
+    def __lockMAC(self, iptables, mac):
+        res = call([iptables, '-t', 'mangle', '-D', self.chain, '-m', 'mac', '--mac-source', mac, '-j', 'RETURN'])
         if res:
             return True
         else:
             return False
-    def unlockDNS(self, ip):
+    def unlockDNS(self, iptables, ip):
         if not call(['iptables', '-t', 'mangle', '-I', self.chain, '-d', ip, '-j', 'RETURN']):
             return False
 
-    def unlockDHCP(self, ip):
+    def unlockDHCP(self, optables, ip):
         if not call(['iptables', '-t', 'mangle', '-I', self.chain, '-d', ip, '-p udp', '--dport', '67:68', '--sport', '67:68', '-j', 'RETURN']):
             return False
 
+    def start(self):
+        self.__start('iptables')
+        self.__start('ip6tables')
+
+    def shutdown(self):
+        self.__shutdown('iptables')
+        self.__shutdown('ip6tables')
+
+    def unlockMAC(self, mac):
+        self.__unlockMAC('iptables', mac)
+        self.__unlockMAC('ip6tables', mac)
+
+    def lockMAC(self, mac):
+        self.__unlockMAC('iptables', mac)
+        self.__unlockMAC('ip6tables', mac)
 
 if __name__ == '__main__':
     pass
